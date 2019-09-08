ec6b31c13423c11ec7d111f057fff3af36499297
ap-- <andreas@poehlmann.io>
2019-09-08 02:05:57 +0200

    ....

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 setlocal
 
-if "%PY3K%" equ "1" goto install
+if not "%PY3K%" == "0" goto install
 :: seabreeze needs windows driver kit 7 on python 2.7
 set INCLUDE=%INCLUDE%;C:\WinDDK\7600.16385.1\inc\api;C:\WinDDK\7600.16385.1\inc\ddk;
 set LIB=%LIB%;C:\WinDDK\7600.16385.1\lib\wxp\i386;
