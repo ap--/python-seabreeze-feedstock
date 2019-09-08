d3b3c844194c9d71321aa9d08a52acb8979434d3
ap-- <andreas@poehlmann.io>
2019-09-08 11:49:25 +0200

    via pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,12 @@
 @echo on
-:: seabreeze needs windows driver kit 7 on python 2.7
-set INCLUDE=%INCLUDE%;C:\WinDDK\7600.16385.1\inc\api;C:\WinDDK\7600.16385.1\inc\ddk;
-set LIB=%LIB%;C:\WinDDK\7600.16385.1\lib\wxp\i386;
 
+if "%PY3K%" == "1" goto installpy3
+:: seabreeze needs windows driver kit 7 on python 2.7 (preinstalled on appveyor)
+"%PYTHON%" -m pip install . --no-deps --no-build-isolation -vv --global-option=build_ext ^
+ --global-option="-Ic:/WinDDK/7600.16385.1/inc/api;c:/WinDDK/7600.16385.1/inc/ddk" ^
+ --global-option="-Lc:/WinDDK/7600.16385.1/lib/wlh/amd64"
+goto end
+
+:installpy3
 "%PYTHON%" -m pip install . --no-deps --no-build-isolation -vv
+:end
